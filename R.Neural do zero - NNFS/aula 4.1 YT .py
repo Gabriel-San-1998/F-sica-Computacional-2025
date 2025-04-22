e0ddfb36e6326d0b313ea0ec94493296d2463345
Gabas <gabriel.camara@ufu.br>
2025-04-22 10:34:13 -0300

    Adicionando mais uma camada

--- R.Neural do zero - NNFS/aula 4.1 YT .py
+++ R.Neural do zero - NNFS/aula 4.1 YT .py
@@ -12,10 +12,16 @@ weigths= [[0.2, 0.8, -0.5, 1], [0.5, -0.91, 0.26, -0.5], [-0.26, -0.27, 0.17, 0.
 
 biases= [2, 3, 0.5]
 
-layer_outputs = []
 
-output= np.dot(inputs, np.array(weigths).T)+biases
-print(output)
+weigths2= [[0.1, -0.14, 0.5], 
+           [-0.5, 0.12, -0.33], 
+           [-0.44, 0.73, -0.13]]
+
+biases2= [-1, 2, -0.5]
+layer1_outputs = np.dot(inputs, np.array(weigths).T)+biases
+
+layer2_outputs= np.dot(layer1_outputs, np.array(weigths2).T)+biases2
+print(layer2_outputs)
 
 #video 4YT pt1
 
---
+++ R.Neural do zero - NNFS/aula 4.2 YT  .py
@@ -0,0 +1,10 @@
+#Vamos criar alguns "lotes" de entrada, pois quanto mais entrada de informação,mais a rede é otimizada
+
+
+import numpy as np
+
+
+X = [[1,2, 3, 2.5],#entradas provenientes de neuronios anteriores 
+        [2, 5, -1, 2],
+        [-1.5, 2.7, 3.3, -0.8]
+        ]
